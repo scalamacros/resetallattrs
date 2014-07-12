d5e066de9c18d4e87cab9c9d1fe4969fc3f45e8d
Eugene Burmako <xeno.by@gmail.com>
2014-07-12 12:46:12 +0300

    makes the signature work

--- resetallattrs/src/main/scala/org/scalamacros/resetallattrs/package.scala
+++ resetallattrs/src/main/scala/org/scalamacros/resetallattrs/package.scala
@@ -6,14 +6,20 @@ import scala.reflect.macros.whitebox.{Context => WhiteboxContext}
 
 package object resetallattrs {
   implicit class ResetAllAttrs(val c: BlackboxContext) {
-    def resetAllAttrs[T, U](tree: T): U = macro Macros.impl
+    def resetAllAttrs[T](tree: T): Any = macro Macros.impl
   }
 }
 
 package resetallattrs {
   object Macros {
     def impl(c: WhiteboxContext)(tree: c.Tree): c.Tree = {
-      ???
+      import c.universe._
+      val q"$_.ResetAllAttrs($context).resetAllAttrs[..$_](...$_)" = c.macroApplication
+      q"new _root_.org.scalamacros.resetallattrs.Helper[$context.type]($context).impl($tree)"
     }
+  }
+
+  class Helper[C <: BlackboxContext](val c: C) {
+    def impl(tree: c.Tree): c.Tree = ???
   }
 }
